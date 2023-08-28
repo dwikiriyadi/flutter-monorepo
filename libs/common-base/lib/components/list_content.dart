7212ba6d127a0cc44b4e6f9a90585dfd7c82fc6f
Dwiki Riyadi <wiki.riyadi@gmail.com>
2023-08-28 14:06:59 +0700

    update list_content

--- libs/common-base/lib/components/list_content.dart
+++ libs/common-base/lib/components/list_content.dart
@@ -35,7 +35,9 @@ class _ListContentState<T> extends State<ListContent<T>> {
     return RefreshIndicator(
       key: widget.refreshIndicatorKey,
       onRefresh: widget.onRefresh,
-      child: (widget.result.data == null && widget.emptyPage != null)
+      child: ((widget.result.isSuccess || widget.result.isIdle) &&
+              widget.result.data == null &&
+              widget.emptyPage != null)
           ? SingleChildScrollView(
               physics: const AlwaysScrollableScrollPhysics(),
               child: SizedBox(
