9a1c6fd11a7b34f1560324c62ca03b7888195ff7
Dwiki Riyadi <wiki.riyadi@gmail.com>
2024-06-04 11:26:47 +0700

    remove create

--- libs/common-base/lib/storage/preferences.dart
+++ libs/common-base/lib/storage/preferences.dart
@@ -1,17 +1,9 @@
-import 'package:common_base/storage/hive_encryption.dart';
 import 'package:hive/hive.dart';
 
 class Preferences {
   late Box box;
 
-  Preferences();
-
-  Future<void> createSecure(String prefName, String secureKey) async {
-    var key = await HiveEncryption.encryptionKey(secureKey);
-    box = await Hive.openBox(prefName, encryptionCipher: HiveAesCipher(key));
-  }
-
-  Future<void> create(String prefName) async {
-    box = await Hive.openBox(prefName);
+  Preferences(prefName) {
+    box = Hive.box(prefName);
   }
 }
